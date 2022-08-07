b7f2367c2dfeec7232a2f1312e00406675387817
Yuan Ruo <liangyuanruo@gmail.com>
2022-08-07 16:09:44 +0800

    feat/map: integrate React Map GL, delete unnecessary boilerplate (#1)

---
+++ pages/components/Map.tsx
@@ -0,0 +1,14 @@
+import ReactMapGL from 'react-map-gl'
+
+export default function Map() {
+  return (<ReactMapGL
+    mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
+    initialViewState={{
+      longitude: 103.83,
+      latitude: 1.35,
+      zoom: 12
+    }}
+    style={{width: '100%', height: '100%', position: 'absolute'}}
+    mapStyle="mapbox://styles/mapbox/streets-v9"
+  />)
+}
