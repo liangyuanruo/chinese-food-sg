befc3ed73c90d159e14648be9e537c297b42dc9b
Yuan Ruo <liangyuanruo@gmail.com>
2022-08-13 22:30:57 +0800

    feat: add GeolocateControl, fix CSS import (#5)

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import dynamic from "next/dynamic";
 import { Suspense } from "react";
+import "mapbox-gl/dist/mapbox-gl.css";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
@@ -19,11 +20,6 @@ const Home: NextPage = () => {
           content="A map to display the types of Chinese food in Singapore"
         />
       </Head>
-      {/* Mapbox GL CSS */}
-      <link
-        href="https://api.tiles.mapbox.com/mapbox-gl-js/v0.44.2/mapbox-gl.css"
-        rel="stylesheet"
-      />
       {/* The Map */}
       <div className={styles.main}>
         <Suspense fallback={`loading...`}>
